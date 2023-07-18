5fef526535dbcda1653cce24cbf161be8f3d68ec
GODOM018 <maria.godoy.-nd2@disney.com>
2023-07-18 17:26:04 -0300

    + customize the textfield as needed. + add onChange callback. - remove `reload` button. + other improvements.

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class _MyAppState extends State<MyApp> {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -10,8 +10,118 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final _controller = editor.HtmlEditorController();
+  final _controller = editor.HtmlEditorController(processNewLineAsBr: false);
+  final _toolBarButtons = const [
+    editor.StyleButtons(),
+    editor.FontButtons(
+      underline: false,
+      subscript: false,
+      superscript: false,
+    ),
+    editor.ListButtons(listStyles: false),
+    editor.InsertButtons(
+      video: false,
+      audio: false,
+    ),
+    // ! This ones are not supported in markdown
+    // editor.ColorButtons(),
+    // editor.ParagraphButtons(),
+    // editor.FontSettingButtons(),
+  ];
+
   String text = 'It is empty';
+
+  editor.HtmlToolbarOptions _getToolBarOptions(BuildContext context) {
+    return editor.HtmlToolbarOptions(
+      buttonBorderWidth: 0.0,
+      dropdownBoxDecoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(
+          color: Colors.grey.shade300,
+        ),
+      ),
+      dropdownItemHeight: 50.0,
+      gridViewHorizontalSpacing: 0.0,
+      renderBorder: true,
+      renderSeparatorWidget: false,
+      textStyle: Theme.of(context).textTheme.bodyMedium,
+      toolbarType: editor.ToolbarType.nativeGrid,
+      defaultToolbarButtons: _toolBarButtons,
+    );
+  }
+
+  Widget _buildPreviewText() {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          color: Colors.white,
+        ),
+        padding: const EdgeInsets.all(8.0),
+        width: double.infinity,
+        child: SingleChildScrollView(
+          controller: ScrollController(),
+          child: SelectableText(text),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildReloadButton() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 32.0),
+      child: FilledButton(
+        onPressed: () async {
+          final html = await _controller.getText();
+          text = html2md.convert(html);
+          if (mounted) {
+            setState(() {});
+          }
+        },
+        child: const Text('reload'),
+      ),
+    );
+  }
+
+  Widget _buildTextEditor(BuildContext context) {
+    return Theme(
+      data: ThemeData.light().copyWith(
+        primaryColor: Colors.blue,
+        toggleButtonsTheme: const ToggleButtonsThemeData(
+          fillColor: Colors.white,
+        ),
+      ),
+      child: Container(
+        constraints: const BoxConstraints(maxHeight: 300.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border.all(
+            color: Colors.grey,
+          ),
+        ),
+        child: editor.HtmlEditor(
+          controller: _controller,
+          htmlEditorOptions: const editor.HtmlEditorOptions(
+            characterLimit: 1000,
+            darkMode: false,
+            hint: "Type here your text",
+          ),
+          htmlToolbarOptions: _getToolBarOptions(context),
+          otherOptions: const editor.OtherOptions(
+            height: 400,
+          ),
+          callbacks: editor.Callbacks(onChangeContent: (String? html) {
+            if (html != null) {
+              text = html2md.convert(html);
+              if (mounted) {
+                setState(() {});
+              }
+            }
+          }),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,38 +131,12 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.deepPurple.shade100,
       ),
       body: Container(
-        padding: const EdgeInsets.only(
-          top: 20,
-          left: 20,
-          right: 20,
-        ),
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
-            editor.HtmlEditor(
-              controller: _controller,
-              htmlEditorOptions: const editor.HtmlEditorOptions(
-                hint: "Type you Text here",
-              ),
-              htmlToolbarOptions: const editor.HtmlToolbarOptions(
-                toolbarType: editor.ToolbarType.nativeGrid,
-              ),
-              otherOptions: const editor.OtherOptions(
-                height: 400,
-              ),
-            ),
+            _buildTextEditor(context),
             const SizedBox(height: 40),
-            SelectableText(text),
-            const SizedBox(height: 40),
-            FilledButton(
-              onPressed: () async {
-                final html = await _controller.getText();
-                text = html2md.convert(html);
-                if (mounted) {
-                  setState(() {});
-                }
-              },
-              child: const Text('reload'),
-            ),
+            _buildPreviewText(),
           ],
         ),
       ),
